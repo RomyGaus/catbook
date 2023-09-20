de37ba21068e3a2596adec0a0bd0fe6335500cab
Romy Gaus <romy.p.gaus@gmail.com>
2023-09-20 08:34:09 +0200

    create catbook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-Tutorial';
+  postTexts = [
+    'Hi, my name is Luis and I am here to find new friends.',
+    'I love snacks!',
+    'I do not really understand what i am doing here. But it is nice :)',
+    'Hello, nice to meet you!',
+    'I hate human beings...they are only good for giving me snacks!',
+    
+  ];
+
+  postImages = [
+    'assets/img/cats/1.jpg',
+    'assets/img/cats/2.jpg',
+    'assets/img/cats/3.jpg',
+    'assets/img/cats/4.jpg',
+    'assets/img/cats/5.jpg',
+    'assets/img/cats/6.jpg',
+    'assets/img/cats/7.jpg',
+    'assets/img/cats/8.jpg',
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CatCardComponent } from './cat-card/cat-card.component';
+import { ProposalsComponent } from './proposals/proposals.component';
+import { ProfileRowComponent } from './profile-row/profile-row.component';
+import { FriendboxComponent } from './friendbox/friendbox.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CatCardComponent,
+    ProposalsComponent,
+    ProfileRowComponent,
+    FriendboxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
